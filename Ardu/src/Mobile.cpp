bf83dcee40869186f84d3db5a8389e9ff50ac9cd
Brainless <s2621019@siheung.hs.kr>
2026-09-14 22:30:27 +0900

    fix: import PIO project

--- Ardu/src/Mobile.cpp
+++
@@ -1,3 +0,0 @@
-#include "Mobile.h"
-#include <Arduino.h>
-
--- Ardu/src/Motor.cpp
+++
@@ -1,7 +0,0 @@
-#include "Motor.h"
-#include <Arduino.h>
-
-Motor::Motor(char p_pwm, char p_fore, char p_back)
-: pin{p_pwm, p_fore, p_back} {
-
-}
